1aa48acdb13dfee581a156081a4ce17434932c96
Andrei <aneagoie1@gmail.com>
2017-01-12 11:37:11 -0500

    imprort tachyons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'; // glue between React and the DOM
 import App from './Containers/App';
 import './index.css';
+import 'tachyons';
 
 ReactDOM.render(
   <App />,
